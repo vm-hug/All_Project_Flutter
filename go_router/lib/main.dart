a373bddb4ff1204e5579245954b15a76cb315034
minhhung <minhhung191205@gmail.com>
2026-07-01 21:02:56 +0700

    initial go router

---
+++ go_router/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/page/my_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomePage());
+  }
+}
